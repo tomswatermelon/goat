bd7cc029607b3ebc5b38b4b3650c8367aedff453
WJ Zhao <weijian.zhao@gmail.com>
2019-02-15 12:29:09 +0800

    ran gulp dev and updated README

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Run `npm install`.
 
 ### Development
 Run `gulp dev`, browser will load the `dist/` folder and auto refresh upon changes.
+
 Run `gulp img`, to pipe all images from `src/img/` to `dist/img/`.
 
 ### Production
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,23 @@
-"use strict";var example=new ExampleModule,example2=new ExampleVendor;Vue.component("component",{props:["message"],template:'\n    <pre id="comp">{{message}}</pre>\n    '});var app=new Vue({el:"#app",data:{message:"Hello World!"},mounted:function(){}});
-"use strict";var ExampleModule=function(){};
+"use strict";
+
+var example = new ExampleModule();
+var example2 = new ExampleVendor();
+console.log('Hi There!');
+Vue.component('component', {
+  props: ['message'],
+  template: "\n    <pre id=\"comp\">{{message}}</pre>\n    "
+});
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello World!'
+  },
+  mounted: function mounted() {
+    console.log('App Mounted');
+  }
+});
+"use strict";
+
+var ExampleModule = function ExampleModule() {
+  console.log("Example module loaded");
+};
